46a574c75ba2f58cb7240995bee8369a56325beb
Ben Charest <58536087+8enji@users.noreply.github.com>
2023-08-07 18:50:39 -0400

    main script

---
+++ main.py
@@ -0,0 +1,6 @@
+from AssetEmail import AssetEmail
+
+if __name__ == '__main__':
+    # create asset email object and initiate loop
+    asset_email = AssetEmail()
+    asset_email.start_email_loop()
